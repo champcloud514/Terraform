5b3200b9890d127eb05b7d57f99e56e7e23db0b1
champcloud514 <akwasiabubonsrah635@gmail.com>
2026-04-13 13:54:08 +0000

    main file added

---
+++ Ch03/main.tf
@@ -0,0 +1,93 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+# ---------------------
+# VPC
+# ---------------------
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+# ---------------------
+# SUBNET (public)
+# ---------------------
+resource "aws_subnet" "main" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-2a"
+  map_public_ip_on_launch = true # ensures EC2 gets a public IP
+}
+
+# ---------------------
+# INTERNET GATEWAY
+# ---------------------
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+# ---------------------
+# ROUTE TABLE
+# ---------------------
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+# ---------------------
+# ROUTE TABLE ASSOCIATION
+# ---------------------
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.public.id
+}
+
+# ---------------------
+# SECURITY GROUP(SSH + HTTP)
+#---------------------
+resource "aws_security_group" "main" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  # SSH (port 22)
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+  # HTTP (port 80)
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# ---------------------
+# EC2 INSTANCE
+# ---------------------
+resource "aws_instance" "my_ec2" {
+  ami                    = "ami-0f5fcdfbd140e4ab7"
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.main.id
+  vpc_security_group_ids = [aws_security_group.main.id]
+
+  tags = {
+    Name = "Terraform-EC2"
+  }
+}
