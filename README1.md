d5178b0211d608cf64ed66643fe8c80e7e51a2f2
champcloud514 <akwasiabubonsrah635@gmail.com>
2026-04-13 13:38:34 +0000

    Ex 1 uploaded

---
+++ README1.md
@@ -0,0 +1,9 @@
+Terraform commands to create 
+1. VPC
+2. Subnet
+3. Security group
+4. Internet gateway
+5. Rout table
+6. Application Load Balancer
+7. Ec2 lauch template
+8. Auto Scaling group
---
+++ main.tf
@@ -0,0 +1,174 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+# --------------------------
+# VPC
+# --------------------------
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-2a"
+  map_public_ip_on_launch = true
+}
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-2b"
+  map_public_ip_on_launch = true
+}
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "public_rt_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id,aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# --------------------------
+# Security Groups
+# --------------------------
+
+# ALB security group
+resource "aws_security_group" "alb_sg" {
+  name        = "alb-sg"
+  description = "Allow HTTP"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 security group for ASG instances
+resource "aws_security_group" "ec2_sg" {
+  name        = "ec2-sg"
+  description = "Allow traffic from ALB"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description     = "Allow HTTP from ALB"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# --------------------------
+# ALB
+# --------------------------
+
+resource "aws_lb" "web_alb" {
+  name               = "web-alb"
+  load_balancer_type = "application"
+  subnets            = [aws_subnet.public_subnet.id,aws_subnet.public_subnet_2.id]
+  security_groups    = [aws_security_group.alb_sg.id]
+}
+
+resource "aws_lb_target_group" "web_tg" {
+  name     = "web-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+  
+  health_check {
+    path = "/"
+    port = "80"
+  }
+}
+
+resource "aws_lb_listener" "http_listener" {
+  load_balancer_arn = aws_lb.web_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.web_tg.arn
+  }
+}
+
+# --------------------------
+# Launch Template
+# --------------------------
+
+resource "aws_launch_template" "web_template" {
+  name_prefix = "web-template"
+
+  image_id               = "ami-0f5fcdfbd140e4ab7"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+
+  user_data = base64encode(<<-EOF
+              #!/bin/bash
+              apt update -y
+              apt install -y nginx
+              echo "<h1>Welcome to the Auto Scaling Group Web Server!</h1>" > /var/www/html/index.html
+              systemctl enable nginx
+              systemctl start nginx
+              EOF
+              )
+}
+
+# --------------------------
+# Auto Scaling Group
+# --------------------------
+
+resource "aws_autoscaling_group" "web_asg" {
+  name                = "web-asg"
+  max_size            = 3
+  min_size            = 1
+  desired_capacity    = 2
+  vpc_zone_identifier = [aws_subnet.public_subnet.id,aws_subnet.public_subnet_2.id]
+
+  launch_template {
+    id      = aws_launch_template.web_template.id
+    version = "$Latest"
+  }
+
+  target_group_arns = [aws_lb_target_group.web_tg.arn]
+
+  tag {
+    key                 = "Name"
+    value               = "ASG-Web-Server"
+    propagate_at_launch = true
+  }
+
+  health_check_type         = "EC2"
+  health_check_grace_period = 300
+}
+
+
